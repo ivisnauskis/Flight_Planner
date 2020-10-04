d6f3d28eaf0944dbf2ed5d09d3c702a28c333768
ivisnauskis <invisnauskis@gmail.com>
2020-10-04 09:07:53 +0300

    Flight service

--- Flight_Planner.Core/Services/IFlightService.cs
+++ Flight_Planner.Core/Services/IFlightService.cs
@@ -1,8 +1,15 @@
 ﻿using Flight_Planner.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Flight_Planner.Core.Services
 {
     public interface IFlightService : IEntityService<Flight>
     {
+        IEnumerable<Flight> GetFlights();
+
+        ServiceResult AddFlight(Flight flight);
+
+        Task<ServiceResult> DeleteFlight(int id);
     }
 }
--- Flight_Planner.Services/FlightService.cs
+++ Flight_Planner.Services/FlightService.cs
@@ -1,4 +1,9 @@
-﻿using Flight_Planner.Core.Models;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Flight_Planner.Core.Models;
 using Flight_Planner.Core.Services;
 using Flight_Planner.Data;
 
@@ -8,6 +13,43 @@ namespace Flight_Planner.Services
     {
         public FlightService(IFlightPlannerDbContext ctx) : base(ctx)
         {
+        }
+
+        public IEnumerable<Flight> GetFlights()
+        {
+            return Get<Flight>();
+        }
+
+        public ServiceResult AddFlight(Flight flight)
+        {
+            if (Exists(flight))
+            {
+                var response = new ServiceResult(false);
+                response.Set(new List<string>() {"Flight must be unique!"});
+                return response;
+            }
+
+            return Create(flight);
+        }
+
+        private bool Exists(Flight flight)
+        {
+            return Query().Any(f => f.ArrivalTime == flight.ArrivalTime &&
+                                    f.Carrier == flight.Carrier &&
+                                    f.DepartureTime == flight.DepartureTime &&
+                                    f.From.AirportCode == flight.From.AirportCode &&
+                                    f.From.City == flight.From.City &&
+                                    f.From.Country == flight.From.Country &&
+                                    f.To.AirportCode == flight.To.AirportCode &&
+                                    f.To.City == flight.To.City &&
+                                    f.To.Country == flight.To.Country);
+        }
+
+
+        public async Task<ServiceResult> DeleteFlight(int id)
+        {
+            var flightToDelete = await GetById(id);
+            return flightToDelete == null ? new ServiceResult(false) : Delete(flightToDelete);
         }
     }
 }
