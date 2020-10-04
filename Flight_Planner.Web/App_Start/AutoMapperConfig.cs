ebdfb5924a01a1bbafe1c0392457959aca794c77
ivisnauskis <invisnauskis@gmail.com>
2020-10-04 21:35:44 +0300

    Automapper config rev

--- Flight_Planner.Web/App_Start/AutoMapperConfig.cs
+++ Flight_Planner.Web/App_Start/AutoMapperConfig.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using Flight_Planner.Core.Models;
 using Flight_Planner.Web.Models;
-using Microsoft.Ajax.Utilities;
+
 
 namespace Flight_Planner.Web
 {
@@ -11,38 +11,19 @@ namespace Flight_Planner.Web
         {
             var config = new MapperConfiguration(cfg =>
             {
+                cfg.CreateMap<FlightRequest, Flight>();
+                cfg.CreateMap<Flight, FlightResponse>();
+
                 cfg.CreateMap<AirportRequest, Airport>()
                     .ForMember(d => d.AirportCode,
-                        s =>
-                            s.MapFrom(p => p.Airport))
+                        s => s.MapFrom(p => p.Airport))
                     .ForMember(d => d.Id,
                         s => s.Ignore());
-                cfg.CreateMap<Airport, AirportRequest>()
-                    .ForMember(d => d.Airport,
-                        s => s.MapFrom(p => p.AirportCode));
-                
-                cfg.CreateMap<FlightRequest, Flight>();
-                cfg.CreateMap<Flight, FlightRequest>();
-                
-                cfg.CreateMap<AirportResponse, AirportRequest>()
-                    .ForMember(d => d.Id,
-                       opt => opt.Ignore());
-                cfg.CreateMap<AirportRequest, AirportResponse>();
-                
-                cfg.CreateMap<FlightRequest, FlightResponse>();
 
                 cfg.CreateMap<Airport, AirportResponse>()
                     .ForMember(m => m.Airport,
                         opt =>
                             opt.MapFrom(s => s.AirportCode));
-
-                cfg.CreateMap<AirportResponse, Airport>()
-                    .ForMember(m => m.Id,
-                        opt => opt.Ignore())
-                    .ForMember(m => m.AirportCode,
-                        opt => opt.MapFrom(s => s.Airport));
-
-                cfg.CreateMap<Flight, FlightResponse>();
             });
             config.AssertConfigurationIsValid();
             var mapper = config.CreateMapper();
